bdd01c93fe198e3235f24f398e34a6008ec98176
carlabilleter <billeter.carla@gmail.com>
2025-03-20 11:24:13 +0100

    Update scenario.py

--- scenario.py
+++ scenario.py
@@ -13,7 +13,7 @@ def generate_scenarios(num_scenarios):
         "max_lidar_dist_main": (5, 5),  # Fixe
         "max_lidar_dist_second": (1, 3),
         "max_episodes": (10,10), 
-        "max_episode_steps": (50,50),
+        "max_episode_steps": (100,500),
         "render_mode": ("human","human")
     }
 
@@ -26,12 +26,12 @@ def generate_scenarios(num_scenarios):
             "grid_size": int(np.round(np.interp(fraction, [0, 1], config_ranges["grid_size"]))),
             "walls_proportion": round(np.interp(fraction, [0, 1], config_ranges["walls_proportion"]), 2),
             "num_dynamic_obstacles": int(np.round(np.interp(fraction, [0, 1], config_ranges["num_dynamic_obstacles"]))),
-            "num_agents": config_ranges["num_agents"][0],
+            "num_agents": int(np.round(np.interp(fraction, [0, 1], config_ranges["num_agents"])))
             "communication_range": int(np.round(np.interp(fraction, [0, 1], config_ranges["communication_range"]))),
-            "max_lidar_dist_main": config_ranges["max_lidar_dist_main"][0],
+            "max_lidar_dist_main": int(np.round(np.interp(fraction, [0, 1], config_ranges["max_lidar_dist_main"]))),
             "max_lidar_dist_second": int(np.round(np.interp(fraction, [0, 1], config_ranges["max_lidar_dist_second"]))), 
-            "max_episodes": config_ranges["max_episodes"][0] ,
-            "max_episode_steps": config_ranges["max_episode_steps"][0],
+            "max_episodes": int(np.round(np.interp(fraction, [0, 1], config_ranges["max_episodes"]))), 
+            "max_episode_steps": int(np.round(np.interp(fraction, [0, 1], config_ranges["max_episode_steps"]))), 
             "render_mode": config_ranges["render_mode"][0]
         }
 
